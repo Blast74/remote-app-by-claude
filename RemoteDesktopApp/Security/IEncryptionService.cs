f669fcf5da532396887913a9b9dec50a6aab01ec
Blast74 <venzo.terence@gmail.com>
2025-07-10 15:46:34 +0200

    Add RemoteDesktopApp/Security/IEncryptionService.cs 1752155192424

---
+++ RemoteDesktopApp/Security/IEncryptionService.cs
@@ -0,0 +1,11 @@
+namespace RemoteDesktopApp.Security;
+
+public interface IEncryptionService
+{
+    string Encrypt(string plainText);
+    string Decrypt(string cipherText);
+    string HashPassword(string password);
+    bool VerifyPassword(string password, string hash);
+    string GenerateSecureKey();
+    byte[] GenerateRandomBytes(int length);
+}
